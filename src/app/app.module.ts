3bbc96ebcf978482d723bfb2bb5566cba198c918
LuisMorais <luismorais252@gmail.com>
2018-03-06 18:27:40 +0000

    Adicionado o footer | TEM ERROS!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,37 @@
+import { UsersComponent } from './components/users/users.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TesteComponent } from './components/teste/teste.component';
-import {FileUploadModule} from 'primeng/fileupload';
-import {ChartModule} from 'primeng/chart';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ChartModule } from 'primeng/chart';
+import { RouterLink, RouterModule } from '@angular/router';
+import {DataListModule} from 'primeng/datalist';
+import {KeyFilterModule} from 'primeng/keyfilter';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {DataTableModule} from 'primeng/datatable';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SidebarComponent,
-    TesteComponent
+    TesteComponent,
+    UsersComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FileUploadModule,
-    ChartModule
+    ChartModule,
+    RouterModule.forRoot([
+    {  path: '', component: TesteComponent },
+    {  path: 'all-users', component: UsersComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/teste/teste.component.ts
+++ src/app/components/teste/teste.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teste.component.css']
 })
 export class TesteComponent implements OnInit {
+
+  myData: any;
   data: any;
   constructor() {
+    // -------- PIE CHART ----------------
     this.data = {
       labels: ['A','B','C'],
         datasets: [{
@@ -24,7 +27,24 @@ export class TesteComponent implements OnInit {
           ]
         }]    
       };
+    // ---------- BAR CHART ------------------- 
+    this.myData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: '#42A5F5',
+          borderColor: '#1E88E5',
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: 'My Second dataset',
+          backgroundColor: '#9CCC65',
+          borderColor: '#7CB342',
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
     }
-  ngOnInit() {
   }
+  ngOnInit() {}    
 }
